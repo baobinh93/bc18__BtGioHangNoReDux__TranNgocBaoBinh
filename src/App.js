986d95482f72b24b94e9ad7d90a2f2187557d202
Bình Trần <binhtran@Binhs-MacBook-Air.local>
2022-03-19 21:23:15 +0700

    xoa file khac

--- src/App.js
+++ src/App.js
@@ -2,15 +2,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import BaiTapGioHang from "./BaiTapGIoHang/BaiTapGioHang";
 
-import DemoRedux from "./DemoRedux/DemoRedux";
-import BaiTapGioHangRedux from "./BaiTapGIoHang-Redux/BaiTapGioHang-Redux";
-
 function App() {
-  return (
-    <BaiTapGioHang />
-    //<DemoRedux />
-    // <BaiTapGioHangRedux />
-  );
+  return <BaiTapGioHang />;
 }
 
 export default App;
